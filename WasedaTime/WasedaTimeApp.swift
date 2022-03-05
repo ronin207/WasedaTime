52194729440c95c074a2b821a7f8af65dea84fba
Takumi Otsuka <takumi.ot0911@gmail.com>
2022-03-05 14:06:08 +0800

    Initial Commit

---
+++ WasedaTime/WasedaTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WasedaTimeApp.swift
+//  WasedaTime
+//
+//  Created by Takumi Otsuka on 5/3/22.
+//
+
+import SwiftUI
+
+@main
+struct WasedaTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
