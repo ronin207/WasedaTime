3b7c18c3390cb70124cb0bacc734f4c27f7a4602
Takumi Otsuka <takumi.ot0911@gmail.com>
2022-03-05 19:31:24 +0800

    Initial Commit

---
+++ WasedaTime/Components/ColorSchemeToggle.swift
@@ -0,0 +1,20 @@
+//
+//  ColorSchemeToggle.swift
+//  WasedaTime
+//
+//  Created by Takumi Otsuka on 5/3/22.
+//
+
+import SwiftUI
+
+struct ColorSchemeToggle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ColorSchemeToggle_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorSchemeToggle()
+    }
+}
