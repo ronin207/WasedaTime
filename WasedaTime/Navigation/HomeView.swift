1b071f321f8bd62ffff8689f815654ab78b8d7fa
Takumi Otsuka <takumi.ot0911@gmail.com>
2022-03-05 20:22:12 +0800

    Added a background image and text color that changes accordingly to the colorScheme

--- WasedaTime/Navigation/HomeView.swift
+++ WasedaTime/Navigation/HomeView.swift
@@ -13,8 +13,34 @@ struct HomeView: View {
     var body: some View {
         NavigationView {
             ZStack {
-                LinearGradient(.main, .side)
-                    .ignoresSafeArea()
+                GeometryReader { proxy in
+                    Image(isOn ? "waseda_dark" : "waseda_light")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: proxy.size.width)
+                        .ignoresSafeArea()
+                        .blur(radius: 3)
+                }
+                
+                ScrollView(.vertical, showsIndicators: false) {
+                    VStack(spacing: 10) {
+                        Image("Logo")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 100, height: 100)
+                        
+                        VStack(spacing: 10) {
+                            Text("WasedaTime")
+                                .font(selection: .optima, size: 40)
+                            
+                            Text("An unofficial app for syllabus searching, classroom finding, and bus schedule checking at Waseda University")
+                                .font(selection: .arial, size: 20)
+                                .multilineTextAlignment(.center)
+                        }
+                        .padding()
+                    }
+                    .foregroundColor(.text1)
+                }
             }
             .toolbar {
                 ToolbarItem(placement: .confirmationAction) {
