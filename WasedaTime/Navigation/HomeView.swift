6e9f28a743d08d06f6efe08fa34b8fbde880f474
Takumi Otsuka <takumi.ot0911@gmail.com>
2022-03-05 19:17:55 +0800

    Initial Commit

---
+++ WasedaTime/Navigation/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  WasedaTime
+//
+//  Created by Takumi Otsuka on 5/3/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
