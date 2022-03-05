8193e461327062e5cddb8b2e5108376d61ea2b24
Takumi Otsuka <takumi.ot0911@gmail.com>
2022-03-05 14:11:10 +0800

    Initial Commit

---
+++ WasedaTime/Extensions/Fonts.swift
@@ -0,0 +1,20 @@
+//
+//  Fonts.swift
+//  WasedaTime
+//
+//  Created by Takumi Otsuka on 5/3/22.
+//
+
+import SwiftUI
+
+struct Fonts: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Fonts_Previews: PreviewProvider {
+    static var previews: some View {
+        Fonts()
+    }
+}
