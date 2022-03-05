c7df91a0a33ec468fa2fd3953980a20ea88ae968
Takumi Otsuka <takumi.ot0911@gmail.com>
2022-03-05 14:50:06 +0800

    Initial Commit

---
+++ WasedaTime/Extensions/Colors.swift
@@ -0,0 +1,20 @@
+//
+//  Colors.swift
+//  WasedaTime
+//
+//  Created by Takumi Otsuka on 5/3/22.
+//
+
+import SwiftUI
+
+struct Colors: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Colors_Previews: PreviewProvider {
+    static var previews: some View {
+        Colors()
+    }
+}
