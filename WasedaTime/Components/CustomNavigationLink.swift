2ca8f30367c9a3adc103a5476f7070a19a24d52b
Takumi Otsuka <takumi.ot0911@gmail.com>
2022-03-06 12:45:12 +0800

    Created a custom navigation link button.

---
+++ WasedaTime/Components/CustomNavigationLink.swift
@@ -0,0 +1,20 @@
+//
+//  CustomNavigationLink.swift
+//  WasedaTime
+//
+//  Created by Takumi Otsuka on 6/3/22.
+//
+
+import SwiftUI
+
+struct CustomNavigationLink: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CustomNavigationLink_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomNavigationLink()
+    }
+}
